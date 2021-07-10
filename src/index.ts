72a983af33a688de1f11c5c501db07442ad81c7d
Thomas Landgraf <45935153+thlandgraf@users.noreply.github.com>
2021-07-10 22:22:36 +0200

    worx

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,11 @@ console.log('Starting up Dashboard Backend')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('Hello World!')
 })
+
+app.use('/', express.static('static'));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
